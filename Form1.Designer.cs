1a7cbfc100e3580dfd04cf552aee84f51dc68a16
olnal <olnalolnal@gmail.com>
2021-12-14 10:30:13 +0200

    Change

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,55 @@ namespace TPR
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.n = new System.Windows.Forms.TextBox();
+            
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введіть кількість років";
+            // 
+            // n
+            // 
+            this.n.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.n.Location = new System.Drawing.Point(355, 43);
+            this.n.Name = "n";
+            this.n.Size = new System.Drawing.Size(124, 38);
+            this.n.TabIndex = 1;
+            
+            // 
+            // proba
+            // 
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.n);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Прийняття рішення";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox n;
+        
     }
 }
 
